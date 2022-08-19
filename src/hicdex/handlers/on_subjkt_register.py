1855b8c2e5159d51481ca214243ce7932f54089a
Yannick Goossens <yannick@ziroh.be>
2022-08-19 11:33:43 +0200

    Delay subjkt ipfs calls

--- src/hicdex/handlers/on_subjkt_register.py
+++ src/hicdex/handlers/on_subjkt_register.py
@@ -2,6 +2,7 @@ import logging
 from typing import Dict
 
 from dipdup.context import HandlerContext
+from dipdup.enums import MessageType
 from dipdup.models import Transaction
 
 import hicdex.models as models
@@ -18,19 +19,25 @@ async def on_subjkt_register(
     registry: Transaction[RegistryParameter, HenSubjktStorage],
 ) -> None:
     addr = registry.data.sender_address
+    _logger.info(f'{addr}')
     holder, _ = await models.Holder.get_or_create(address=addr)
 
     name = fromhex(registry.parameter.subjkt)
+    _logger.info(f'{name}')
     metadata_file = fromhex(registry.parameter.metadata)
+    _logger.info(f'{metadata_file}')
     metadata: Dict[str, str] = {}
 
     holder.name = name
     holder.metadata_file = metadata_file
     holder.metadata = metadata
 
-    if metadata_file.startswith('ipfs://'):
+    level = ctx.get_tzkt_datasource("tzkt_mainnet").get_channel_level(MessageType.head)
+    if registry.data.level > level - 200 and metadata_file.startswith('ipfs://'):
         _logger.info("Fetching IPFS metadata")
         holder.metadata = await fetch_metadata_ipfs(ctx, holder.metadata_file)
+    else:
+        _logger.info("Skipping metadata fetch")
 
     holder.description = holder.metadata.get('description', '')
 
